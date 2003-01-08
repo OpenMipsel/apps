cc54635bf325e1a2905afc7921b1650489ad4671
TripleDES <TripleDES>
2003-01-08 15:55:21 +0000

    rfmod changes

--- tuxbox/enigma/src/setup_rfmod.h
+++ tuxbox/enigma/src/setup_rfmod.h
@@ -14,9 +14,23 @@ class eZapRFmodSetup: public eWindow
 	eButton *abort, *ok;
 	eStatusBar *status;
 
+	eCheckbox* TestPatternEnable;
+	eCheckbox* SoundEnable;
+	
+	eListBox<eListBoxEntryText> *SoundSubcarrier;
+	eListBox<eListBoxEntryText> *Channel;
+	eListBox<eListBoxEntryText> *FineTune;
+	
 private:
 	void okPressed();
 	void abortPressed();
+	void TestPatternEnable_selected();
+	void SoundEnable_selected();
+	void SoundSubcarrier_selected(eListBoxEntryText* entry);
+	void Channel_selected(eListBoxEntryText* entry);
+	void FineTune_selected(eListBoxEntryText* entry);
+
+	int SFD,SO,DIV;
 
 public:
 	eZapRFmodSetup();
