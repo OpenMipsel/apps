65f8ae8fa56960459a4f1304538635a088634e49
alexvrs <alexvrs>
2003-06-06 10:15:33 +0000

    The code of the sign of degree in EPG is corrected

--- tuxbox/enigma/src/channelinfo.cpp
+++ tuxbox/enigma/src/channelinfo.cpp
@@ -220,7 +220,7 @@ void eChannelInfo::getServiceInfo( const eServiceReferenceDVB& service )
 		cname.setFlags(RS_FADE);
 		cname.resize( eSize( clientrect.width()/8*7-4, clientrect.height()/3) );
 		int opos=service.getDVBNamespace().get()>>16;
-		copos.setText(eString().sprintf("%d.%d\xAF%c", abs(opos / 10), abs(opos % 10), opos>0?'E':'W') );
+		copos.setText(eString().sprintf("%d.%d\xB0%c", abs(opos / 10), abs(opos % 10), opos>0?'E':'W') );
 		EITEvent *e = 0;
 		e = eEPGCache::getInstance()->lookupEvent(service);
 		if (e)  // data is in cache...
