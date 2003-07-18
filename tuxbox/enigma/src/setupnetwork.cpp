a6375ded19a46504d8f29634ec538d772e058eeb
ghostrider <ghostrider>
2003-07-18 17:02:53 +0000

    so is better

--- tuxbox/enigma/src/setupnetwork.cpp
+++ tuxbox/enigma/src/setupnetwork.cpp
@@ -201,16 +201,16 @@ int getRejectFlags()
 	}
 	int flags=0;
 	char *p = getOptionString(buf, "REJECT_WWW=");
-	if ( p && strstr(p,"REJECT_WWW=yes") )
+	if ( p && !strnicmp(p,"yes",3) )
 		flags|=1;
 	p = getOptionString(buf, "REJECT_TELNET=");
-	if ( p && strstr(p,"REJECT_TELNET=yes") )
+	if ( p && !strnicmp(p,"yes",3) )
 		flags|=2;
 	p = getOptionString(buf, "REJECT_SAMBA=");
-	if ( p && strstr(p,"REJECT_SAMBA=yes") )
+	if ( p && !strnicmp(p,"yes",3) )
 		flags|=4;
 	p = getOptionString(buf, "REJECT_FTP=");
-	if ( p && strstr(p,"REJECT_FTP=yes") )
+	if ( p && !strnicmp(p,"yes",3) )
 		flags|=8;
 	fclose(f);
 	return flags;
